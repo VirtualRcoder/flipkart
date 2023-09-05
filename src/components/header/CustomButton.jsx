d68fd07e5eb4a2817b64d449eeeaf1a150c27bc0
Shubham Sharma <shubhamsh2820@gmail.com>
2023-09-05 12:20:35 +0000

    Added Menu to header and made header responsive

--- src/components/header/CustomButton.jsx
+++ src/components/header/CustomButton.jsx
@@ -7,15 +7,25 @@ import { ShoppingCart } from "@mui/icons-material";
 
 import { DataContext } from '../../context/DataProvider';
 
-const Wrapper = styled(Box)`
-    display: flex;
-    margin: 0 3% 0 auto;
-    & > button, & > p, & > div{
-        margin-right: 40px;
-        font-size: 16px;
-        align-items: center;
+const Wrapper = styled(Box)(({theme})=>({
+    display: 'flex',
+    margin: '0 3% 0 auto',
+    '& > button, & > p, & > div': {
+        marginRight: 40,
+        fontSize: 16,
+        alignItems: 'center'
+    },
+    [theme.breakpoints.down('md')]:{
+        display: 'block'
     }
-`;
+}))
+
+const Container = styled(Box)(({ theme })=>({
+    display: 'flex',
+    [theme.breakpoints.down('md')]:{
+        display: 'block'
+    }
+}))
 
 const LoginButton = styled(Button)`
     color: #2874f0;
@@ -46,10 +56,10 @@ function CustomButton(){
             }
             <Typography style={{marginTop: 3, width: 135}}>Become a Seller</Typography>
             <Typography style={{marginTop: 3}}>More</Typography>
-            <Box style={{display: 'flex'}}>
+            <Container>
                 <ShoppingCart/>
                 <Typography>Cart</Typography>
-            </Box>
+            </Container>
             <LoginDialog open={open} setOpen= {setOpen}/>
         </Wrapper>
     );
